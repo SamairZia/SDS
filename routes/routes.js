775a6cf9b815d34524fad71c85d2f2302b014018
asgharmusani <asgharmusani@gmail.com>
2018-03-08 15:20:44 +0500

    Version 1.4\nNew update with Node Modules

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,43 @@
-module.exports = function(express, app){
+module.exports = function(express, app, passport)
+{
 	var router = express.Router(),
 		bodyParser = require('body-parser')
 	
 	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({
+	app.use(bodyParser.urlencoded(
+	{
 		extended: true
-		})
-	);
+	}));
+
 	
-	router.get('/', function(req, res, next){
+	router.get('/', function(req, res, next)
+	{
 		res.render('index',{titlePage: 'Welcome to Shakir Dental Clinic'});
 	})
 	
-	router.post('/login', function(req, res, next){
-		console.log(req.body.email);
-		res.render('main',{titlePage: 'Home Page', user:req.body} );
+	router.post('/login', passport.authenticate('local-signin', 
+	{
+            successRedirect: '/main',
+ 
+            failureRedirect: '/'
+	}))
+	
+	router.get('/main', isLoggedIn, function(req, res, next)
+	{
+		res.render('main',{titlePage: 'Home Page', user:req.body});
 	})
 	
+	function isLoggedIn(req, res, next) 
+	{
+        if (req.isAuthenticated()) 
+		{
+			next();
+		}
+		else 
+		{
+			res.redirect('/');
+		}
+    }
+		
 	app.use('/', router);
 }
--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	path = require('path');
-	
+	path = require('path'),
+	env = require('dotenv').load(),
+	session = require('express-session'),
+	passport = require('passport');
+
+//Models
+var models = require("./models");
+
+//app environment
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized:true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
 
-require('./routes/routes.js')(express, app);
+//routes
+require('./routes/routes.js')(express, app, passport);
 
-server.listen(3002, function(){
-	console.log('Server working...');
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
 });
 
+//strategy
+require('./config/passport/passport.js')(passport, models.user);
+
+server.listen(3000, function(){
+    console.log('Server working...');
+    console.log('Mode ' + env);
+});
