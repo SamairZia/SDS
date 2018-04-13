1438af8b9ac62eac3a99de3b4f15be8ca5ecfa0c
asgharmusani <asgharmusani@gmail.com>
2018-04-13 18:10:50 +0500

    final

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -4,6 +4,6 @@ exports.signup = function (req, res, next){
 	res.render('signup');
 }
 
-exports.login = function (req,res, next){
+exports.login = function (req, res, next){
 	res.render('main',{titlePage: 'Home Page', user:req.body});
 }
