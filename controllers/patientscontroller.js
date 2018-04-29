f177741fdd1d8781e70e6f1f3a52198f52df14bb
asgharmusani <asgharmusani@gmail.com>
2018-04-29 17:36:04 +0500

    Adding patient finalized

--- controllers/patientscontroller.js
+++ controllers/patientscontroller.js
@@ -1,6 +1,6 @@
-var sequelize = require("sequelize")
-	exports = module.exports = {}
-	models = require("../models");
+var models = require("../models"),
+	sequelize = models.sequelize,
+	exports = module.exports = {},
 	Patients = models.patients,
 	PatientQA = models.patientqa,
 	PatientQAFamily = models.patientqafamily,
@@ -9,59 +9,94 @@ var sequelize = require("sequelize")
 
 exports.addPatient = function(req,res,next){
 	console.log("Patient controller is working");
-	var keys = Object.keys(req.body);
-	var chkKeys = [];
-	keys.forEach(function(key, index){
-		if(req.body[key] === 'on')
-		{
-			chkKeys.push(key);
+	var regNo = req.body.regNo;
+	var reqKeys = Object.keys(req.body);
+	var reqQA = [];
+	var reqQAFamily = [];
+	var dataForPatientQA = [];
+	var dataForPatientQAFamily = [];
+	for (let problemQA of reqKeys){
+		if(req.body[problemQA] === "on" && !problemQA.startsWith("fam")){
+			reqQA.push(problemQA);
 		}
-	})
-	console.log(chkKeys);
-	PatientQA.findAll({
-		where:{
-			problem:chkKeys
+		if(problemQA.startsWith("fam")){
+			reqQAFamily.push(problemQA);
 		}
 	}
-	).then(function (data){
-		//console.log(data);
-	})
 	
-	// sequelize.transaction(function(t){
-		// return Patients.create({
-			// regno 			: req.body.regNo,
-			// datereg 		: req.body.date,
-			// name 			: req.bodyname,
-			// parentpname		: req.body.parentpname,
-			// houseaddress	: req.body.houseaddress,
-			// workaddress		: req.body.workaddress,
-			// maritalstatus	: req.body.mstatus,
-			// occupation		: req.body.occupation,
-			// phonenumber		: req.body.tel,
-			// reference		: req.body.reference,
-			// name			: req.body.name,
-			// age 			: req.body.age,
-			// sex				: req.body.sex,
-			// balance			: 0
-		// }, {transaction: t}).then(function(patient){
-			
-			// return PatientHealth.bulkCreate({
-				// regno: req.body.regNo,
-				// healthid: 
-					
-				// })
-				// hospitalization
-			// }, {transaction: t})
-			
-		// });
-	// }).then(function (result){
-		// res.send({
-			// status: 'OK',
-			// data: data
-		// });
-	// }).catch(function (err){
+	PatientQA.findAll({
+		where:{
+			problem:reqQA
+		}
+	}).then(function (data){
+		for (var i=0; i<reqQA.length; i++){
+			for(var j=0; j<data.length; j++){
+				if (reqQA[i] === data[j].problem){
+					dataForPatientQA.push({
+						regno : regNo,
+						healthid : data[j].id
+					})
+					break
+				}
+			}
+		}
+	});
+	
+	PatientQAFamily.findAll({
+		where:{
+			problem:reqQAFamily
+		}
+	}).then(function (data){
+		for (var i=0; i<reqQAFamily.length; i++){
+			for(var j=0; j<data.length; j++){
+				if (reqQAFamily[i] === data[j].problem){
+					dataForPatientQAFamily.push({
+						regno : regNo,
+						healthfamilyid : data[j].id
+					})
+					break
+				}
+			}
+		}
+		
+	});
+	
+	var dataForPatient = {
+			regno 			: req.body.regNo,
+			datereg 		: req.body.date,
+			name 			: req.bodyname,
+			parentpname		: req.body.parentpname,
+			houseaddress	: req.body.houseaddress,
+			workaddress		: req.body.workaddress,
+			maritalstatus	: req.body.mstatus,
+			occupation		: req.body.occupation,
+			phonenumber		: req.body.tel,
+			reference		: req.body.reference,
+			name			: req.body.name,
+			age 			: req.body.age,
+			sex				: req.body.sex,
+			balance			: 0,
+			hospitalization : req.body.hospitalization
+		}
+	
+	sequelize.transaction(function(t){
+		return Patients.create(dataForPatient, {transaction: t})
+		.then(function(patient){
+			console.log(dataForPatientQA);
+			return PatientHealth.bulkCreate(dataForPatientQA, {transaction: t})
+			.then(function(somedata){
+				console.log(dataForPatientQAFamily);
+				return PatientHealthFamily.bulkCreate(dataForPatientQAFamily, {transaction: t})
+			})
+		})
+	}).then(function (result){
+		console.log(result);
+		res.send({
+			status: 'OK',
+		})
+	}).catch(function (err){
 		// console.log(err)
-	// });
+	})
 	
 }
 
