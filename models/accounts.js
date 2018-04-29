4e01f209a205ccacff324ed11e138bad2d2d9cc2
asgharmusani <asgharmusani@gmail.com>
2018-04-29 17:36:29 +0500

    for sequelize-auto

--- models/accounts.js
+++ models/accounts.js
@@ -1,34 +1,34 @@
-/* jshint indent: 2 */
+/* jshint indent: 1 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('accounts', {
-    regno: {
-      type: DataTypes.INTEGER(11).UNSIGNED,
-      allowNull: false,
-      primaryKey: true,
-      references: {
-        model: 'patients',
-        key: 'regno'
-      }
-    },
-    trno: {
-      type: DataTypes.INTEGER(11).UNSIGNED,
-      allowNull: false,
-      references: {
-        model: 'treatments',
-        key: 'trno'
-      }
-    },
-    date: {
-      type: DataTypes.DATEONLY,
-      allowNull: true
-    },
-    amount: {
-      type: DataTypes.INTEGER(11).UNSIGNED,
-      allowNull: true
-    }
-  }, {
-    tableName: 'accounts',
-	timestamps: false
-  });
+	return sequelize.define('accounts', {
+		regno: {
+			type: DataTypes.INTEGER(11).UNSIGNED,
+			allowNull: false,
+			primaryKey: true,
+			references: {
+				model: 'patients',
+				key: 'regno'
+			}
+		},
+		trno: {
+			type: DataTypes.INTEGER(11).UNSIGNED,
+			allowNull: false,
+			references: {
+				model: 'treatments',
+				key: 'trno'
+			}
+		},
+		date: {
+			type: DataTypes.DATEONLY,
+			allowNull: true
+		},
+		amount: {
+			type: DataTypes.INTEGER(11).UNSIGNED,
+			allowNull: true
+		}
+	}, {
+		tableName: 'accounts',
+		timestamps: false
+	});
 };
