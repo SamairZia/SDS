21fd658efb62e1dfcdec9bd6cd4f08f28530ec7a
asgharmusani <asgharmusani@gmail.com>
2018-05-08 18:15:28 +0500

    changed

--- public/js/script.js
+++ public/js/script.js
@@ -81,25 +81,15 @@ document.getElementById('dashDate').innerHTML = m + "/" + d + "/" + y ;
 // } 
 
 function validateForm() {
-    var x = document.getElementById('userName').value;
-    var y = document.getElementById('userEmail').value;
-    var z = document.getElementById('userPass').value;
+    var userName = document.getElementById('userName').value;
+    var userPass = document.getElementById('userPass').value;
     // var x = document.forms["loginform"]["uname"].value;
-    if (x == "" || z == "") {
+    if (userName == "" || userPass == "") {
         alert("All fileds must be filled out.");
         document.getElementById("errorLogin").innerHTML = "All fields must be filled out!"
         return false;
     }
-    else {
-        var data = {"name": x ,"email": y , "pass": z};
-        var dataString = JSON.stringify(data);
-        //localStorage.setItem("testing" , dataString);
-        xmlhttp = new XMLHttpRequest();
-        
-        xmlhttp.open("POST" , "/logn" , true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send(dataString);
-        document.getElementById("errorLogin").innerHTML = ""        
+    else {        
         return true;
     }
 }
@@ -111,52 +101,59 @@ var successAddPatient = document.getElementById('successModal');
 var closeModalAddPatient = document.getElementById('myModal');
 
 formSubmitAddPatient.onsubmit = function(e) {
-    var name = document.getElementById('name').value;
-    var pname = document.getElementById('pname').value;
-    var houseAddress = document.getElementById('houseaddress').value;
-    var workAddress = document.getElementById('workaddress').value;
-    var mstatus = document.getElementById('mstatus').value;
-    var occupation = document.getElementById('occupation').value;
-    var tel = document.getElementById('telNo').value;
-    var reference = document.getElementById('reference').value;
-    var age = document.getElementById('age').value;
-    var sex = document.getElementById('sex').value;
-    var regNo = document.getElementById('regNo').value;
-
-    //Getting ids of Patient Health Questionnaire
+	var date 			= document.getElementById('date').value;
+    var name 			= document.getElementById('name').value;
+    var pname 			= document.getElementById('paname').value;
+    var houseAddress 	= document.getElementById('houseAddress').value;
+    var workAddress 	= document.getElementById('workAddress').value;
+    var mstatus 		= document.getElementById('mstatus').value;
+    var occupation 		= document.getElementById('occupation').value;
+    var tel 			= document.getElementById('telNo').value;
+    var reference 		= document.getElementById('reference').value;
+    var age 			= document.getElementById('age').value;
+    var sex 			= document.getElementById('sex').value;
+    var regNo 			= document.getElementById('regNo').value;
+	var hospitalization = document.getElementById('hospitalization').value;
+    
+	//Getting ids of Patient Health Questionnaire
     // 1. Personal Habits
-    var smoking= document.getElementById('smoking').checked;
-    var tobacco = document.getElementById('tobacco').checked;
-    var betelNuts = document.getElementById('betelNuts').checked;
-    var alcohol = document.getElementById('alcohol').checked;
-    //2. Medical Profile
-    var diabetes = document.getElementById('diabetes').checked;
-    var pepticUlcer = document.getElementById('pepticUlcer').checked;
-    var hypertension = document.getElementById('hypertension').checked;
-    var asthma = document.getElementById('asthma').checked;
-    var tuberculosis = document.getElementById('tuberculosis').checked;
-    var hepatitis = document.getElementById('hepatitis').checked;
-    var thyroidDisorder = document.getElementById('thyroidDisorder').checked;
-    var renalDisease = document.getElementById('renalDisease').checked;
-    var skinDisease = document.getElementById('skinDisease').checked;
-    var bleedingDisorder = document.getElementById('bleedingDisorder').checked;
-    var ischemicHeartDisease = document.getElementById('ischemicHeartDisease').checked;
-    var valvularHeartDisease= document.getElementById('valvularHeartDisease').checked;
-    //Reason for Previous Hospitalization
-    var reason = document.getElementById('reason').value;
-    //3. Allergies
-    var penicillin = document.getElementById('penicillin').checked;
-    var anesthesia = document.getElementById('anesthesia').checked;
-    var aspirin = document.getElementById('aspirin').checked;
-    var latexProduct = document.getElementById('latexProduct').checked;
-    //4. Family History
-    var famdiabetes = document.getElementById('famdiabetes').checked;
-    var famhypertension = document.getElementById('famhypertension').checked;
+    var smoking 		= document.getElementById('smoking').checked;
+    var tobacco 		= document.getElementById('tobacco').checked;
+    var betelNuts 		= document.getElementById('betelNuts').checked;
+    var alcohol 		= document.getElementById('alcohol').checked;
+    
+	//2. Medical Profile
+    var diabetes 				= document.getElementById('diabetes').checked;
+    var pepticUlcer 			= document.getElementById('pepticUlcer').checked;
+    var hypertension 			= document.getElementById('hypertension').checked;
+    var asthma 					= document.getElementById('asthma').checked;
+    var tuberculosis 			= document.getElementById('tuberculosis').checked;
+    var hepatitis 				= document.getElementById('hepatitis').checked;
+    var thyroidDisorder 		= document.getElementById('thyroidDisorder').checked;
+    var renalDisease 			= document.getElementById('renalDisease').checked;
+    var skinDisease 			= document.getElementById('skinDisease').checked;
+    var bleedingDisorder 		= document.getElementById('bleedingDisorder').checked;
+    var ischemicHeartDisease 	= document.getElementById('ischemicHeartDisease').checked;
+    var valvularHeartDisease 	= document.getElementById('valvularHeartDisease').checked;
+    
+	//Reason for Previous Hospitalization
+    var reason 					= document.getElementById('reason').value;
+    
+	//3. Allergies
+    var penicillin 				= document.getElementById('penicillin').checked;
+    var anesthesia 				= document.getElementById('anesthesia').checked;
+    var aspirin 				= document.getElementById('aspirin').checked;
+    var latexProduct 			= document.getElementById('latexProduct').checked;
+    
+	//4. Family History
+    var famdiabetes 			= document.getElementById('famdiabetes').checked;
+    var famhypertension 		= document.getElementById('famhypertension').checked;
     var famischemicHeartDisease = document.getElementById('famischemicHeartDisease').checked;
-    var fambleedingDisorder = document.getElementById('fambleedingDisorder').checked;
-    //5. For females
-    var pregnancy = document.getElementById('pregnancy').checked;
-    var breastFeeding = document.getElementById('breastFeeding').checked;
+    var fambleedingDisorder 	= document.getElementById('fambleedingDisorder').checked;
+    
+	//5. For females
+    var pregnancy 				= document.getElementById('pregnancy').checked;
+    var breastFeeding 			= document.getElementById('breastFeeding').checked;
 
     e.preventDefault();
     if(name == "" || pname == "" || houseAddress == "" || occupation == "" || tel == "" || age == "" || regNo == ""){
@@ -165,44 +162,44 @@ formSubmitAddPatient.onsubmit = function(e) {
     }
     else {
         var infoPatient = {
-			name : name,
-			pname : pname,
-            houseAddress : houseAddress,
-            workAddress : workAddress,
-			mstatus : mstatus,
-			occupation: occupation,
-            tel : tel,
-            reference : reference,
-			age : age,
-			sex : sex,
-            regNo : regNo,
-            smoking : smoking,
-            tobacco : tobacco,
-            betelNuts : betelNuts,
-            alcohol : alcohol,
-            diabetes : diabetes,
-            pepticUlcer : pepticUlcer,
-            hypertension : hypertension,
-            asthma : asthma,
-            tuberculosis : tuberculosis,
-            hepatitis : hepatitis,
-            thyroidDisorder : thyroidDisorder,
-            renalDisease : renalDisease,
-            skinDisease : skinDisease,
-            bleedingDisorder : bleedingDisorder,
-            ischemicHeartDisease : ischemicHeartDisease,
-            valvularHeartDisease : valvularHeartDisease,
-            reason : reason,
-            penicillin : penicillin,
-            anesthesia : anesthesia,
-            aspirin : aspirin,
-            latexProduct : latexProduct,
-            famdiabetes : famdiabetes,
-            famhypertension : famhypertension,
+			name 					: name,
+			pname 					: pname,
+            houseAddress		 	: houseAddress,
+            workAddress 			: workAddress,
+			mstatus 				: mstatus,
+			occupation	 			: occupation,
+            tel 					: tel,
+            reference 				: reference,
+			age 					: age,
+			sex 					: sex,
+            regNo 					: regNo,
+            smoking 				: smoking,
+            tobacco 				: tobacco,
+            betelNuts 				: betelNuts,
+            alcohol 				: alcohol,
+            diabetes 				: diabetes,
+            pepticUlcer 			: pepticUlcer,
+            hypertension 			: hypertension,
+            asthma 					: asthma,
+            tuberculosis 			: tuberculosis,
+            hepatitis 				: hepatitis,
+            thyroidDisorder 		: thyroidDisorder,
+            renalDisease 			: renalDisease,
+            skinDisease 			: skinDisease,
+            bleedingDisorder 		: bleedingDisorder,
+            ischemicHeartDisease 	: ischemicHeartDisease,
+            valvularHeartDisease 	: valvularHeartDisease,
+            reason 					: reason,
+            penicillin 				: penicillin,
+            anesthesia 				: anesthesia,
+            aspirin 				: aspirin,
+            latexProduct 			: latexProduct,
+            famdiabetes 			: famdiabetes,
+            famhypertension 		: famhypertension,
             famischemicHeartDisease : famischemicHeartDisease,
-            fambleedingDisorder : fambleedingDisorder,
-            pregnancy : pregnancy,
-            breastFeeding :breastFeeding
+            fambleedingDisorder 	: fambleedingDisorder,
+            pregnancy 				: pregnancy,
+            breastFeeding 			: breastFeeding
 			};
         
         var xhttp = new XMLHttpRequest();
@@ -248,7 +245,7 @@ formSubmitAddPatient.onsubmit = function(e) {
          };
          var xhttpAppointment = new XMLHttpRequest();
 
-         xhttpAppointment.open("POST" , "/patients/add" , true);
+         xhttpAppointment.open("POST" , "/main/appointments/add" , true);
          xhttpAppointment.setRequestHeader("Content-type", "application/json");     
          xhttpAppointment.send(JSON.stringify(infoAppointment));
          xhttpAppointment.onreadystatechange = function(){
@@ -289,7 +286,7 @@ formSubmitAddTreatment.onsubmit = function(e) {
         };
         var xhttpTreatment = new XMLHttpRequest();
 
-        xhttpTreatment.open("POST" , "/patients/add" , true);
+        xhttpTreatment.open("POST" , "/main/treatments/add" , true);
         xhttpTreatment.setRequestHeader("Content-type", "application/json");     
         xhttpTreatment.send(JSON.stringify(infoTreatment));
         xhttpTreatment.onreadystatechange = function(){
