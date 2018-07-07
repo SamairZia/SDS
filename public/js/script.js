b756ac11af6f7fbf4b59748dcaab0ef539d1d083
Samair Zia <samairzia1995@gmail.com>
2018-07-07 21:01:56 +0500

    added

--- public/js/script.js
+++ public/js/script.js
@@ -255,26 +255,9 @@ formSubmitAddPatient.onsubmit = function(e) {
          return true;
      }
 	 
-	 //this is to get recent appointment number
-	 //use it after add appointment window loads
-	 var xhtp = new XMLHttpRequest();
-	 xhtp.open("GET", "main/appointment/getAppNo");
-	 xhtp.onload = function(){
-		 var myObj = JSON.parse(xhtp.responseText);
-		 document.getElementById('appointmentNo').value = myObj.appNo;
-	 }; 
-	 xhtp.send();
+	//moved code at the bottom
 	 
-	 //this is to get the patient name
-	 //use it when focus is moved from Patient name
-	 var getPatientName = new XMLHttpRequest();
-	 getPatientName.open("GET", "main/patients/getPatientName/" +pRegNoApp);
-	 getPatientName.onload = function(){
-		 var myObj = JSON.parse(getPatientName.responseText);
-		 document.getElementById('patientName').value = myObj.patientName;
-	 }; 
 	 
-	 getPatientName.send();
  }
 
 // Add treatment stuff
@@ -353,3 +336,38 @@ searchPatient.onclick = function(e){
     //     }
     // };
 }
+
+//Chunk from AddAppointmnet stuff
+ //this is to get recent appointment number
+     //use it after add appointment window loads
+     var first = document.getElementById('pRegNoApp');
+     first.onfocusout = function() {loadAppNo()};
+    //   = first.value;
+
+     function loadAppNo() {
+         console.log('sssss')
+         alert('load');
+        //  sec = pRegNoApp;
+     var xhtp = new XMLHttpRequest();
+     var pRegNoApp = document.getElementById('pRegNoApp').value;
+	 xhtp.open("GET", "main/appointment/getAppNo" +pRegNoApp);
+	 xhtp.onload = function(){
+		 var myObj = JSON.parse(xhtp.responseText);
+         document.getElementById('appointmentNo').value = myObj.appNo;
+		//  document.getElementById('patientName').value = myObj.patientName;         
+	 }; 
+     
+     var getPatientName = new XMLHttpRequest();
+	 getPatientName.open("GET", "main/patients/getPatientName/" +pRegNoApp);
+	 getPatientName.onload = function(){
+         alert("patient name triggered");
+		 var myObj = JSON.parse(getPatientName.responseText);
+		 document.getElementById('patientName').value = myObj.patientName;
+     };
+     xhtp.send();
+	 getPatientName.send();
+    }
+
+     //this is to get the patient name
+	 //use it when focus is moved from Patient name
+	      
