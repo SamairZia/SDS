a239f6aa7f67c440eb982fe501c67a5c76c2801b
asgharmusani <asgharmusani@gmail.com>
2018-08-28 23:56:44 +0500

    added getRegNo +comments

--- routes/patient.js
+++ routes/patient.js
@@ -2,14 +2,20 @@ module.exports = function (express, app){
 	var router = express.Router(),
 		patientcontroller = require('../controllers/patientcontroller.js')
 		
-	
+	//opens the patient template
 	router.get('/', patientcontroller.patient);
 	
+	//adds patient to the DB
 	router.post('/add', patientcontroller.addPatient);
 	
+	//returns Name of the patient based on the regno
 	router.get('/getPatientName/:regNo', patientcontroller.getPatientName);
 	
+	//returns all patient
 	router.get('/getPatientAll', patientcontroller.getPatientAll);
+	
+	//returns the last regNo+1
+	router.get('/getRegNo', patientcontroller.getRegNo);
 	
 	app.use('/main/patient', router);
 }
